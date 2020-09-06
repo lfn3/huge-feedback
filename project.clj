5823ad8f586a865ffe9fb6c98ce9437054f60845
lfn3 <liam@lfn3.net>
2020-09-06 14:32:00 +0100

    Cut 0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject huge-feedback "0.2.0-SNAPSHOT"
+(defproject huge-feedback "0.2.0"
   :dependencies [;clj
                  [org.clojure/clojure "1.10.0"]
                  [cheshire "5.8.1"]
